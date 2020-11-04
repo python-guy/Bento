44e3678a503bb8c4a482f0cf96a8a069573a17fe
Jian Wen <jianwen815@gmail.com>
2020-11-04 11:04:45 +0800

    Polished up greetings.js -Finished Added api key and changed longtitude + latitude in weather.js -Finished

--- js/greeting.js
+++ js/greeting.js
@@ -6,12 +6,12 @@ var hour = today.getHours();
 var name = 'Jian Wen';
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
-var gree3 = 'Good afternoon ';
-var gree4 = 'Good evening ';
-var gree5 = 'Good evening ';
-var gree6 = 'Good evening ';
+var gree1 = 'Go to Sleep, ';
+var gree2 = 'Good morning, ';
+var gree3 = 'Good afternoon, ';
+var gree4 = 'Good evening, ';
+var gree5 = 'Good evening, ';
+var gree6 = 'Good evening, ';
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '9eb873f3b5c9e99da65f6661cc6a592a';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 1.352083;
+    let longitude = 103.819839;
 
     getWeather(latitude, longitude);
 }
